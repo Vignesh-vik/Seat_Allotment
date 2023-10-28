6065c77d601c97183ab86f2ae21726d162e7894d
Vignesh S <vignesh1255hg@gmail.com>
2023-10-29 00:33:28 +0530

    Students dets like perc and allotment obtained

--- CollegeEntry.py
+++ CollegeEntry.py
@@ -1,9 +1,27 @@
 # clg,stud = int(input("clg:")),int(input("stud:"))
 clg =3
-i=1
+stud=3
 seats=[]
-while i <= clg:
-    a=int(input("Enter seats:\n"))
-    seats.append(a)
-    i+=1
-print(seats)
+def Num_seats():
+    i=1
+    while i <= clg:
+        print("Enter number seats in college",i,":")
+        a=int(input(""))
+        seats.append(a)
+        i+=1
+Num_seats()
+dets={}
+def Studs_dets():
+    i=1
+    while i<=stud:
+        print("Details of student",i,":")
+        perc = input("enter percentage:")
+        alot = input("enter Allotment:")
+        alot = alot.split(" ")
+        # print(alot)
+        dets = [perc,alot]
+        print(dets)
+Studs_dets()
+
+
+
